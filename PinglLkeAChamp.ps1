bb2d0dab7e0ba6d7a7e1c99eda3fba33ec5770a7
RohiRIK <108658615+RohiRIK@users.noreply.github.com>
2022-10-20 20:32:17 +0300

    Update PinglLkeAChamp.ps1

--- PinglLkeAChamp.ps1
+++ PinglLkeAChamp.ps1
@@ -1,46 +1,60 @@
-﻿$answer= Read-Host "This script will send pings to any devices that found in the list and will export the running devices into to CSV file.
- do you wish to proceed? Yes/No" 
-Start-Sleep -seconds 0.8
-
-echo "Made by ROHI RIKMAN for lernning Propose, heve FUN troubleshooting"
-Start-Sleep -seconds 1
-
-
-if ($answer -eq "Yes")
+param($L,$E)
+if ($L -eq $null -Or $E -eq $null)
     {
-    $Path= Read-Host "Pleas insert the full name and Path of the file with the devices list"
-    $Path =$Path.Replace("""","")
-    $devicess = Get-Content -Path $Path
-
-    $exportfile = Read-Host "How do you wont to call the exported file?"
-    Add-Content -Path "$exportfile.csv" -Value '"Host-name","IP-address"'
-
-    ### Funny loding 
-    for ($i = 1; $i -le 100; $i++ ) 
-        {
-            Write-Progress -Activity "Start PINGING, Pleas wait for loding the Csv file" -Status "$i% Complete:" -PercentComplete $i
-            Start-Sleep -Milliseconds 15
-        }
-
-    $list=@()
-     foreach ($devices in $devicess)
-         {
-         if (Test-Connection  $devices -Count 1 -ErrorAction SilentlyContinue)
-            {
-                $ping = ping -4 $devices
-                $ip = $ping.Item(2)
-                $ip = $ip.Substring(11,11)
-                $list+=("$devices,$ip")
-                $list | Add-Content -Path "$exportfile.csv" 
-                Write-Host "Append $devices to CSV..." -ForegroundColor Green
-            }
-        else
-            {
-                Write-Host "The $devices is down so no Append to CSV" -ForegroundColor Red
-            }
-         }
+    echo "Pleas insert the Parmeter fo the scripts 
+    -L for Choosing List fo Devices
+    -E for Coosing the name of the file to export
+    Pleas insert the Full Path"
     }
+
 else
-{
-echo "have a nice day"
-}
+    {
+    $answer= Read-Host "This script will send pings to any devices that found in the list and will export the running devices into to CSV file.
+    do you wish to proceed? Yes/No" 
+    Start-Sleep -seconds 0.8
+
+    echo "Made by ROHI RIKMAN for lernning Propose, heve FUN troubleshooting"
+    Start-Sleep -seconds 1
+
+
+    if ($answer -eq "Yes")
+        {
+    
+        #$Path= Read-Host "Pleas insert the full name and Path of the file with the devices list"
+        $L =$L.Replace("""","")
+        $devices = Get-Content -Path $L
+
+        #$exportfile = Read-Host "How do you wont to call the exported file?"
+        Add-Content -Path "$E.csv" -Value '"Host-name","IP-address"'
+
+        for ($i = 1; $i -le 100; $i++ ) 
+            {
+                Write-Progress -Activity "Start PINGING, Pleas wait for loading the Csv file" -Status "$i% Complete:" -PercentComplete $i
+                Start-Sleep -Milliseconds 15
+            }
+
+        $list=@()
+            foreach ($device in $devices)
+                {
+                if (Test-Connection  $device -Count 1 -ErrorAction SilentlyContinue)
+                {
+                    $ping = ping -4 $device
+                    $ip = $ping.Item(2)
+                    $ip = $ip.Substring(11,11)
+                    $list+=("$device,$ip")
+                    #$list | Add-Content -Path "$E.csv" 
+                    #$devices,$ip | Add-Content -Path "$E.csv" 
+                    Write-Host "Append $device to CSV..." -ForegroundColor Green
+                }
+            else
+                {
+                    Write-Host "The $device is down so no Append to CSV" -ForegroundColor Red
+                }
+                }
+        }
+    else
+    {
+    echo "have a nice day"
+    }
+    $list | Add-Content -Path "$E.csv"
+    }
